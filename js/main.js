1c93319fd15f6c679282bbe15b11994e13dc726c
Ahmed <ahmedmoselhi7@gmail.com>
2019-02-26 17:59:09 -0500

    rs

--- js/main.js
+++ js/main.js
@@ -1,141 +1,291 @@
-/*
- * Dew Templates (Personal Portofolio)
- * Created by : vavelo
- * Version : 1.1
- */
+/** 
+ * ===================================================================
+ * main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+	/*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });       
+
+  	})
 
 
-/* INDEX OF CONTENTS JAVASCRIPT
-==================================================
-  XX. PRELOADER
-  01. NAVIGATION
-  02. MOBILE NAVIGATION
-  03. PARALLAX BACKGROUND EFFECT
-  04. TEXT ROTATE
-  05. HOME ARROW DOWN
-  06. SMOTH SCROLL
-  07. PORTFOLIO FILTER IMAGE
-  08. TESTIMONIAL SLIDER
-  09. ANIMATION EFFECT  
-*/
+  	/*---------------------------------------------------- */
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+
+  	}, 100);
 
 
-(function ($) {
-	'use strict';
+	/*---------------------------------------------------- */
+	/* FitVids
+	------------------------------------------------------ */ 
+  	$(".fluid-video-wrapper").fitVids();
 
-	jQuery(document).ready(function () {
+
+	/*---------------------------------------------------- */
+	/* Owl Carousel
+	------------------------------------------------------ */ 
+	$("#owl-slider").owlCarousel({
+        navigation: false,
+        pagination: true,
+        itemsCustom : [
+	        [0, 1],
+	        [700, 2],
+	        [960, 3]
+	     ],
+        navigationText: false
+    });
 
 
-		/* XX. PRELOADER
-		==================================================*/
+	/*----------------------------------------------------- */
+	/* Alert Boxes
+  	------------------------------------------------------- */
+	$('.alert-box').on('click', '.close', function() {
+	  $(this).parent().fadeOut(500);
+	});	
 
-		$(window).on('load', function () {
-			$("#status").fadeOut();
-			$("#preloader").delay(500).fadeOut("slow");
+
+	/*----------------------------------------------------- */
+	/* Stat Counter
+  	------------------------------------------------------- */
+   var statSection = $("#stats"),
+       stats = $(".stat-count");
+
+   statSection.waypoint({
+
+   	handler: function(direction) {
+
+      	if (direction === "down") {       		
+
+			   stats.each(function () {
+				   var $this = $(this);
+
+				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+				   	duration: 4000,
+				   	easing: 'swing',
+				   	step: function (curValue) {
+				      	$this.text(Math.ceil(curValue));
+				    	}
+				  	});
+				});
+
+       	} 
+
+       	// trigger once only
+       	this.destroy();      	
+
+		},
+			
+		offset: "90%"
+	
+	});	
+
+
+	/*---------------------------------------------------- */
+	/*	Masonry
+	------------------------------------------------------ */
+	var containerProjects = $('#folio-wrapper');
+
+	containerProjects.imagesLoaded( function() {
+
+		containerProjects.masonry( {		  
+		  	itemSelector: '.folio-item',
+		  	resize: true 
 		});
-
-
-		/* 01. NAVIGATION
-		==================================================*/
-
-		$(window).on('scroll', function () {
-			if ($(window).scrollTop() > 100) {
-				$('#navigation').addClass('nav-bg');
-			} else {
-				$('#navigation').removeClass('nav-bg');
-			}
-		});
-
-
-		/* 02. MOBILE NAVIGATION
-		==================================================*/
-
-		$(document).on('click', '.navbar-collapse.in', function (e) {
-			if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-				$(this).collapse('hide');
-			}
-		});
-
-		$('body').scrollspy({
-			target: '.navbar-collapse',
-			offset: 195
-		});
-
-
-		/* 03. PARALLAX BACKGROUND EFFECT
-		==================================================*/
-
-		var parallax = function () {
-			$(window).stellar();
-		};
-
-		$(function () {
-			parallax();
-		});
-
-
-		/* 04. TEXT ROTATE
-		==================================================*/
-		$(document).on('ready', function () {
-			$('#text-rotator').rotatetext({
-				fadeSpeed: 500,
-				pauseSpeed: 2000
-			});
-		});
-
-
-		/* 05. HOME ARROW DOWN
-		==================================================*/
-
-		var arrowBounce = function () {
-			var arrow = $(".arrow");
-			if (arrow.hasClass("lift")) {
-				arrow.removeClass("lift");
-			} else {
-				arrow.addClass("lift");
-			}
-		};
-
-		setInterval(arrowBounce, 800);
-
-
-		/* 06. SMOTH SCROLL
-		==================================================*/
-
-		$('a.smoth-scroll').on('click', function (e) {
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 50
-			}, 1000);
-			e.preventDefault();
-		});
-
-
-		/* 07. PORTFOLIO FILTER IMAGE
-		==================================================*/
-		$('#port-image').mixItUp();
-
-
-		/* 08. TESTIMONIAL SLIDER
-		==================================================*/
-
-		$('.flexslider').flexslider({
-			animation: "fade",
-			directionNav: false
-		});
-
-
-		/* 09. ANIMATION EFFECT
-		==================================================*/
-
-		AOS.init({
-			duration: 1200,
-			once: true,
-			disable: 'mobile'
-		});
-
-
 
 	});
+
+
+	/*----------------------------------------------------*/
+	/*	Modal Popup
+	------------------------------------------------------*/
+   $('.item-wrap a').magnificPopup({
+
+      type:'inline',
+      fixedContentPos: false,
+      removalDelay: 300,
+      showCloseBtn: false,
+      mainClass: 'mfp-fade'
+
+   });
+
+   $(document).on('click', '.popup-modal-dismiss', function (e) {
+   	e.preventDefault();
+   	$.magnificPopup.close();
+   });
+
+	
+	/*-----------------------------------------------------*/
+  	/* Navigation Menu
+   ------------------------------------------------------ */  
+   var toggleButton = $('.menu-toggle'),
+       nav = $('.main-navigation');
+
+   // toggle button
+   toggleButton.on('click', function(e) {
+
+		e.preventDefault();
+		toggleButton.toggleClass('is-clicked');
+		nav.slideToggle();
+
+	});
+
+   // nav items
+  	nav.find('li a').on("click", function() {   
+
+   	// update the toggle button 		
+   	toggleButton.toggleClass('is-clicked'); 
+   	// fadeout the navigation panel
+   	nav.fadeOut();   		
+   	     
+  	});
+
+
+   /*---------------------------------------------------- */
+  	/* Highlight the current section in the navigation bar
+  	------------------------------------------------------ */
+	var sections = $("section"),
+	navigation_links = $("#main-nav-wrap li a");	
+
+	sections.waypoint( {
+
+       handler: function(direction) {
+
+		   var active_section;
+
+			active_section = $('section#' + this.element.id);
+
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		}, 
+
+		offset: '25%'
+	});
+
+
+	/*---------------------------------------------------- */
+  	/* Smooth Scrolling
+  	------------------------------------------------------ */
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});  
+  
+
+   /*---------------------------------------------------- */
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */ 
+	$('input, textarea, select').placeholder()  
+
+
+  	/*---------------------------------------------------- */
+	/*	contact form
+	------------------------------------------------------ */
+
+	/* local validation */
+	$('#contactForm').validate({
+
+		/* submit via ajax */
+		submitHandler: function(form) {
+
+			var sLoader = $('#submit-loader');
+
+			$.ajax({      	
+
+		      type: "POST",
+		      url: "inc/sendEmail.php",
+		      data: $(form).serialize(),
+		      beforeSend: function() { 
+
+		      	sLoader.fadeIn(); 
+
+		      },
+		      success: function(msg) {
+
+	            // Message was sent
+	            if (msg == 'OK') {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').hide();
+	               $('#contactForm').fadeOut();
+	               $('#message-success').fadeIn();   
+	            }
+	            // There was an error
+	            else {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').html(msg);
+		            $('#message-warning').fadeIn();
+	            }
+
+		      },
+		      error: function() {
+
+		      	sLoader.fadeOut(); 
+		      	$('#message-warning').html("Something went wrong. Please try again.");
+		         $('#message-warning').fadeIn();
+
+		      }
+
+	      });     		
+  		}
+
+	});
+
+
+ 	/*----------------------------------------------------- */
+  	/* Back to top
+   ------------------------------------------------------- */ 
+	var pxShow = 300; // height on which the button will show
+	var fadeInTime = 400; // how slow/fast you want the button to show
+	var fadeOutTime = 400; // how slow/fast you want the button to hide
+	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+
+   // Show or hide the sticky footer button
+	jQuery(window).scroll(function() {
+
+		if (!( $("#header-search").hasClass('is-visible'))) {
+
+			if (jQuery(window).scrollTop() >= pxShow) {
+				jQuery("#go-top").fadeIn(fadeInTime);
+			} else {
+				jQuery("#go-top").fadeOut(fadeOutTime);
+			}
+
+		}		
+
+	});		
 
 })(jQuery);
